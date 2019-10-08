245b0b7f8be6a1ab18f58db26119507b21e7f6ea
Chris Sherman <chris.sherman@outlook.com>
2019-10-08 11:55:40 -0400

    Add is validZipCode function.

--- src/Region.js
+++ src/Region.js
@@ -34,43 +34,33 @@ module.exports = class Region {
   static getMatchingRegion(obj, zipCodeProperty, regionsToMatch) {
     if (typeof obj !== 'object' || obj === null) {
       throw new TypeError(
-        'obj must be a JavaScript object. Invalid value: ' +
+        'Parameter obj must be a JavaScript object. Invalid value: ' +
           JSON.stringify(obj) +
-          '.'
+          '.',
       );
     }
 
+    // Ensure zipCodeProperty is a string before using it.
     if (typeof zipCodeProperty !== 'string') {
       throw new TypeError(
-        'zipCodeProperty must be a string. Invalid value: ' +
+        'Parameter zipCodeProperty must be a string. Invalid value: ' +
           JSON.stringify(zipCodeProperty) +
-          '.'
+          '.',
       );
     }
 
     if (!obj.hasOwnProperty(zipCodeProperty)) {
-      throw new Error(`obj must have a property named ${zipCodeProperty}.`);
-    }
-
-    if (typeof obj[zipCodeProperty] !== 'string') {
-      throw new TypeError(
-        `obj must have a property named ${zipCodeProperty} ` +
-          `with a string value`
-      );
-    }
-
-    if (!ConfigService.regexZipCodePattern.test(obj[zipCodeProperty])) {
       throw new Error(
-        'Zip code values must be a string with five digits or ' +
-          'five digits with a dash followed by four digits. ' +
-          `Invalid value: ${obj[zipCodeProperty]}`
+        `Parameter obj must have a property named ${zipCodeProperty}.`,
       );
     }
+
+    isValidZipCode(obj[zipCodeProperty], true);
 
     if (!Array.isArray(regionsToMatch)) {
       throw new TypeError(
-        'regionsToMatch must be an Array of Regions. Invalid ' +
-          `value: ${JSON.stringify(regionsToMatch)}.`
+        'regionsToMatch must be an Array. Invalid ' +
+          `value: ${JSON.stringify(regionsToMatch)}.`,
       );
     }
 
@@ -116,28 +106,50 @@ function validateZipCodes(zipCodes) {
   if (!(zipCodes instanceof Set)) {
     throw new TypeError(
       'zipCodes must be a Set of numeric strings. ' +
-        `Invalid value: ${JSON.stringify(zipCodes)}.`
+        `Invalid value: ${JSON.stringify(zipCodes)}.`,
     );
   }
 
   zipCodes.forEach(zipCode => {
-    const errorMessage =
-      'Each zip code must be a numeric string of length five. ' +
-      `Invalid value: ${zipCode}.`;
-
-    if (typeof zipCode !== 'string') {
-      throw new TypeError(errorMessage);
-    }
-
-    const regexNumeric = /[0-9]/;
-    if (!regexNumeric.test(zipCode)) {
-      throw new TypeError(errorMessage);
-    }
-
-    if (zipCode.length !== 5) {
-      throw new RangeError(errorMessage);
-    }
+    isValidZipCode(zipCode, true);
   });
+
+  return true;
+}
+
+/**
+ * Test a zip code for validity and throw an Error on failure.
+ *
+ * @param {string} zipCode Zip code to test.
+ * @param {boolean} throwOnInvalid Throw an error if the zip code fails validation.
+ *
+ * @returns {boolean} True if the zip code passes validation.
+ *
+ * @throws {Error} If the zip code fails validation and throwOnInvalid is true.
+ */
+function isValidZipCode(zipCode, throwOnInvalid = false) {
+  if (typeof zipCode !== 'string') {
+    if (throwOnInvalid) {
+      throw new TypeError(
+        'Paramter zipCode must be of type string. ' +
+          `Invalid value: ${JSON.stringify(zipCode)}`,
+      );
+    }
+
+    return false;
+  }
+
+  if (!ConfigService.regexZipCodePattern.test(zipCode)) {
+    if (throwOnInvalid) {
+      throw new Error(
+        'Paramter zipCode must be either five digits or ' +
+          'five digits with a dash followed by four digits. ' +
+          `Invalid value: ${zipCode}`,
+      );
+    }
+
+    return false;
+  }
 
   return true;
 }
